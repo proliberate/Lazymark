08733341a4d6dddc5b84e868b299e9565ac28ba2
Andrew Carroll <mr.andrewcarroll@gmail.com>
2013-04-03 18:16:43 -0400

    rename main->element, create parser
    parser not complete

--- element.py
+++ element.py
@@ -1,13 +1,13 @@
 class Element:
 
-	def __init__(self):
+	def __init__(self,name="",cat=[],iden=[],attr=[],depth=0,content="",closed=True):
 		self.name = ""
-		self.cat = []
-		self.iden = []
-		self.attr = []
-		self.level = 0
-		self.content = ""
-		self.open = False;
+		self.cat = cat
+		self.iden = iden
+		self.attr = attr
+		self.depth = depth
+		self.content = content
+		self.closed = closed
 
 	def __enter__(self):
 		return self
@@ -35,7 +35,8 @@ class Element:
 		cat = "class='{}'".format(" ".join([str(x) for x in self.cat]))
 		iden = "id='{}'".format(" ".join([str(x) for x in self.iden]))
 		attr = " ".join("{}={}".format(attr[0],attr[1]) for attr in self.attr)
+		depth = "\t" * self.depth
 		content = self.content
-		closetag = "" if self.open else "</{}>".format(name)
-		tag = "<{}>{}{}".format(" ".join(x for x in [name,cat,iden,attr]),content,closetag)
+		closetag = "" if not self.closed else "</{}>".format(name)
+		tag = "{}<{}>{}{}".format(depth," ".join(x for x in [name,cat,iden,attr]),content,closetag)
 		print tag
---
+++ parser.py
@@ -0,0 +1,12 @@
+class Parser:
+	
+	def __init__(self,filepath):
+		self.line = 0
+		self.depth = 0
+		self.filepath = filepath
+
+	def parseLine(self,line):
+			
+
+	def parse(self,element):
+		with Element()
